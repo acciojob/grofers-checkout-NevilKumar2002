0a3c7e881546a7342df3e043aa49c966f1b9685d
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2023-09-19 18:04:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,19 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	  var table = document.getElementById("table"), sumVal = 0;
+            
+            for(var i = 1; i < table.rows.length; i++)
+            {
+                sumVal = sumVal + parseInt(table.rows[i].cells[1].innerHTML);
+            }
+	 document.getElementById("val").innerHTML = "Sum Value = " + sumVal;
+            console.log(sumVal); 
+            
   
 };
+
+
 
 getSumBtn.addEventListener("click", getSum);
 
